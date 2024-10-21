1100d4a6a9a64bb3768b4e23b39ea48d94a6c14d
Lyamin Timofey <162110108+HaventlyGuard@users.noreply.github.com>
2024-10-21 22:19:25 +0700

    add class Ship

---
+++ Ship.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle
+{
+    public class Ship
+    {
+        private int Size { get;}
+        private int X { get; }
+        private int Y { get; }
+        private bool IsHorizontal { get; set; }
+
+        public Ship(int size, int x, int y, bool isHorizontal)
+        {
+            Size = size;
+            X = x;
+            Y = y;
+            IsHorizontal = isHorizontal;
+        }
+
+
+        public void ChangeDirection()
+        {
+            if (IsHorizontal) {
+                IsHorizontal = false; 
+            }
+            else IsHorizontal = true;
+        }
+        public void DestroyShip()
+        {
+            Console.WriteLine($"Корабль размером {Size} был уничтожен");
+        }
+    }
+}
